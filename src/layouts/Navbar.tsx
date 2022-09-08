1ba5e69942b39bb0381baf7ec5a8418cadbb84c4
Artin GH <artinghazizadeh86@gmail.com>
2022-09-08 13:00:48 +0430

    Convert navbar animations to react-transition-group

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { useState } from "react";
 import styles from "../styles/layouts/Navbar.module.css";
 import { useRouter } from "next/router";
+import { CSSTransition } from "react-transition-group";
 
 const menuItems = [
   { name: "HOME", href: "/" },
@@ -16,48 +17,64 @@ const Navbar: React.FC = () => {
 
   return (
     <nav
-      className={`text-white ${
-        styles.navbar
-      } flex justify-between items-center nav-font
-                     ${isOpen ? "active" : ""}`}
+      className={`text-white ${styles.navbar} flex justify-between items-center nav-font`}
     >
       <figure className={styles.logo}>
         <img src="/images/shared/logo.svg" />
       </figure>
       <hr
         className={`flex-grow ml-14 -mr-7 h-px border-white border-opacity-25
-                      ${styles.line} z-20`}
+                        ${styles.line} z-20`}
       />
-      <ul
-        className={`${styles.menu} flex bg-white bg-opacity-5
-                      rounded-l-sm backdrop-blur-4xl`}
-      >
-        {menuItems.map((item, i) => (
-          <li className={styles.menuItem} key={i}>
-            <Link href={item.href}>
-              <a
-                className={`${styles.menuLink} ${
-                  activeUrl === item.href ? "active" : ""
-                }`}
-              >
-                <span className="number">{i}</span>
-                {item.name}
-              </a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <button
-        type="button"
-        className={`${styles.hamburger} z-20`}
-        onClick={() => {
-          setIsOpen(!isOpen);
+      <CSSTransition
+        in={isOpen}
+        timeout={{enter: 0, exit: 600}}
+        classNames={{
+          enter: styles.menuAnimEnter,
+          enterDone: styles.menuAnimEnterDone,
+          exit: styles.menuAnimExit,
+          exitActive: styles.menuAnimExitActive,
         }}
       >
-        {[0, 1, 2].map((i) => (
-          <div key={i} className={styles.hamburgerLine}></div>
-        ))}
-      </button>
+        <ul className={`${styles.menu} flex rounded-l-sm`}>
+          {menuItems.map((item, i) => (
+            <li className={styles.menuItem} key={i}>
+              <Link href={item.href}>
+                <a
+                  className={`${styles.menuLink} ${
+                    activeUrl === item.href ? "active" : ""
+                  }`}
+                >
+                  <span className="number">{i}</span>
+                  {item.name}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </CSSTransition>
+      <CSSTransition
+        in={isOpen}
+        timeout={0}
+        classNames={{
+          enter: styles.hamburgerAnimEnter,
+          enterDone: styles.hamburgerAnimEnterDone,
+          exit: styles.hamburgerAnimExit,
+          exitDone: styles.hamburgerAnimExitDone,
+        }}
+      >
+        <button
+          type="button"
+          className={`${styles.hamburger} z-20`}
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          {[0, 1, 2].map((i) => (
+            <div key={i} className={styles.hamburgerLine}></div>
+          ))}
+        </button>
+      </CSSTransition>
     </nav>
   );
 };
