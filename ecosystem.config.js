b3058fe2f1b755150b142f7bf24d8376c4128e5d
Artin GH <artinghazizadeh86@gmail.com>
2023-03-01 23:43:51 +0330

    Add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      script: "npm start",
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: "key.pem",
+      user: "root",
+      host: "s16.v2center.ga",
+      ref: "origin/main",
+      repo: "git@github.com:Artin-GH/space-tourism-website.git",
+      path: "/root",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "npm install && npm build && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
+};
